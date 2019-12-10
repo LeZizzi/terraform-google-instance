f757a16141d2393142eebe597044cbccbd28c7b7
olivier@nephele.ie <Moufa,123>
2019-12-10 11:21:20 +0000

    simplify

--- main.tf
+++ main.tf
@@ -1,29 +1,24 @@
-resource "google_compute_instance" "default" {
+resource "google_compute_instance" "webserver" {
   name         = "rbuild0"
   machine_type = "f1-micro"
-  region       = "europe-west2"
   zone         = "europe-west2-a"
-
   tags = ["web"]
+  metadata_startup_script = "apt-get update && apt-get install -y nginx"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-9-stretch-v20191121"
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-
-  }
 
   network_interface {
     network = "default"
-
     access_config {
       // Ephemeral IP
     }
   }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8"
+    }
+  }
+
 
   metadata = {
     sshkeys= "debian:${file(var.ssh_public_key_filepath)}"
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
 output "external_ip" {
-  value = "${google_compute_instance.default.network_interface.0.access_config.0}"
+  value = google_compute_instance.default.network_interface[0].access_config[0]
+
+  //value = "${google_compute_instance.default.network_interface.0.access_config.0}"
+
+
 }
+
